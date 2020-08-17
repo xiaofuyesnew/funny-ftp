e36675c8c3d3091e06d7a53d40d2f30c8c895543
xiaofuyesnew <xiaofuyesnew@qq.com>
2020-08-17 17:21:44 +0800

    提交更新

--- src/index.js
+++ src/index.js
@@ -17,91 +17,91 @@ const fs = require('fs')
 // }
 
 class FunnyFTP {
-    constructor(config) {
-        if (config) {
-            if (!(config.host || config.user || config.pass || config.local)) {
-                let errArr = []
-                if (!config.host) {
-                    errArr.push('host')
-                }
-                if (!config.user) {
-                    errArr.push('user')
-                }
-                if (!config.pass) {
-                    errArr.push('pass')
-                }
-                if (!config.local) {
-                    errArr.push('local')
-                }
-                throw new Error(`no necessary value in key [${errArr.join(',')}]`)
-            }
-            this.config = {
-                host: config.host,
-                port: config.port || 21,
-                user: config.user,
-                pass: config.pass,
-                local: config.local
-            }
-        } else {
-            throw new Error('need config Object')
+  constructor(config) {
+    if (config) {
+      if (!(config.host || config.user || config.pass || config.local)) {
+        let errArr = []
+        if (!config.host) {
+          errArr.push('host')
         }
+        if (!config.user) {
+          errArr.push('user')
+        }
+        if (!config.pass) {
+          errArr.push('pass')
+        }
+        if (!config.local) {
+          errArr.push('local')
+        }
+        throw new Error(`no necessary value in key [${errArr.join(',')}]`)
+      }
+      this.config = {
+        host: config.host,
+        port: config.port || 21,
+        user: config.user,
+        pass: config.pass,
+        local: config.local
+      }
+    } else {
+      throw new Error('need config Object')
     }
-    connect() {
+  }
+  connect() {
 
-        const self = this
-        let flag = false
-        const client = new net.Socket()
-        this.client = client
+    const self = this
+    let flag = false
+    const client = new net.Socket()
+    this.client = client
 
-        console.log(`connect to ${this.config.host}:${this.config.port}...`)
+    console.log(`connect to ${this.config.host}:${this.config.port}...`)
 
-        const dataServer = net.createServer(socket => {
-            console.log('data conn')
-            self.dataSocket = socket
-            socket.on('data', data => {
-                console.log('ok')
-                console.log(data.toJSON())
-            })
-        })
-        client.connect(this.config.port, this.config.host, () => {
-            console.log('connected successfully')
-            console.log(client.localPort)
-            dataServer.listen(client.localPort + 1, self.config.local)
-        })
-        
-        client.on('data', msg => {
-            console.log(msg.toString().replace('\r\n', ''))
-            let code = +msg.toString().substring(0, 3)
-            // login
-            if (code === 220) {
-                client.write(`USER ${self.config.user}\r\n`)
-            }
-            if (code === 331) {
-                client.write(`PASS ${self.config.pass}\r\n`)
-            }
-            if (code === 230) {
-                // login successful
-                client.write('TYPE I\r\n')
-                flag = true
-            }
-            if (code === 200) {
-                if (flag) {
-                    client.write(`PORT ${self.config.local.split('.').join(',')},${parseInt((client.localPort + 1) / 256)},${(client.localPort + 1) % 256}\r\n`)
-                    flag = false
-                } else {
-                    client.write('PWD\r\n')
-                }
-            }
-            if (code === 257) {
-                client.write(`RETR Web.config\r\n`)
-            }
-        })
+    const dataServer = net.createServer(socket => {
+      console.log('data conn')
+      self.dataSocket = socket
+      socket.on('data', data => {
+        console.log('ok')
+        console.log(data.toJSON())
+      })
+    })
+    client.connect(this.config.port, this.config.host, () => {
+      console.log('connected successfully')
+      console.log(client.localPort)
+      dataServer.listen(client.localPort + 1, self.config.local)
+    })
 
-        client.on('end', () => {
-            console.log('结束')
-            // client.write('CWD 2018\r\n')
-        })
-    }
+    client.on('data', msg => {
+      console.log(msg.toString().replace('\r\n', ''))
+      let code = +msg.toString().substring(0, 3)
+      // login
+      if (code === 220) {
+        client.write(`USER ${self.config.user}\r\n`)
+      }
+      if (code === 331) {
+        client.write(`PASS ${self.config.pass}\r\n`)
+      }
+      if (code === 230) {
+        // login successful
+        client.write('TYPE I\r\n')
+        flag = true
+      }
+      if (code === 200) {
+        if (flag) {
+          client.write(`PORT ${self.config.local.split('.').join(',')},${parseInt((client.localPort + 1) / 256)},${(client.localPort + 1) % 256}\r\n`)
+          flag = false
+        } else {
+          client.write('PWD\r\n')
+        }
+      }
+      if (code === 257) {
+        client.write(`RETR Web.config\r\n`)
+      }
+    })
+
+    client.on('end', () => {
+      console.log('结束')
+      // client.write('CWD 2018\r\n')
+    })
+  }
 }
 
 module.exports = FunnyFTP
