229a7d3779c75e862afb62c415f3018d438b7b25
xiaofuyesnew <xiaofuyesnew@qq.com>
2021-06-17 00:33:33 +0800

    提交更新

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const { Socket, createServer } = require("net");
 const fs = require("fs");
 const path = require("path");
 
-let client = null
+let client, localAddress, localPort, server;
 
 // 遍历文件结构
 
@@ -25,39 +25,70 @@ const mapDir = async (target, handle) => {
 };
 
 // mapDir('dist', file => console.log(`upload:${file}`))
-const handleConnect = async () => {
-  console.log('已连接服务端')
-  console.log(`localAddress: ${client.localAddress}`)
-  setTimeout(() => {
-    // client.destroy()
-  }, 60000)
+const handleConnect = () => {
+  console.log("已连接服务端");
+  console.log(`localAddress: ${client.localAddress}`);
+  localAddress = client.localAddress;
+  localPort = client.localPort;
+  // setTimeout(() => {
+  //   client.destroy();
+  // }, 5000);
 };
 
-const handleData = async (e) => {
-  console.log('接受数据')
-  console.log(e.toString());
+const handleData = (e) => {
+  const msg = e.toString().replace("\r\n", "");
+  const code = +msg.split(" ")[0];
+  console.log(msg);
+
+  if (code === 220) {
+    return client.write(`USER ${user}\r\n`);
+  }
+
+  if (code === 331) {
+    return client.write(`PASS ${pass}\r\n`);
+  }
+
+  if (code === 230) {
+    return client.write("TYPE I\r\n");
+  }
+
+  if (msg === "200 Type set to I") {
+    return client.write(
+      `PORT ${localAddress.split(".").join(",")},${Math.floor(
+        (localPort + 1) / 256
+      )},${(localPort + 1) % 256}\r\n`
+    );
+  }
+
+  if (msg === '200 Port command successful') {
+    console.log('端口配置完成')
+    // return client.write('PWD\r\n')
+    return client.write('CWD /dev/auto\r\n')
+  }
+
+  // if (code === 257) {
+  //   return client.write('CWD /dev')
+  // }
 };
 
-const handleClose = async (e) => {
-  console.log('链接关闭')
+const handleClose = (e) => {
+  console.log("链接关闭");
   console.log(e);
 };
 
-const handleEnd = async () => {
-  console.log('链接结束')
+const handleEnd = () => {
+  console.log("链接结束");
 };
 
-const createClient = async (port, host) => {
-  console.log("创建客户端...");
-  const client = new Socket();
-  return client
+const createClient = (port, host) =>
+  new Socket()
     .connect(port, host, handleConnect)
     .on("data", handleData)
     .on("close", handleClose)
     .on("end", handleEnd);
-};
 
-client = createClient(port, host)
+console.log("创建客户端...");
+client = createClient(port, host);
 
 const funny = async () => {};
 
